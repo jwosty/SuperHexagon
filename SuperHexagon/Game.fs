507141886acfbfcd4ddc8e51f4eb70f2b4a115a4
jwosty <jwosty@users.noreply.github.com>
2014-07-04 14:44:28 -0600

    Added more sections

--- SuperHexagon/Game.fs
+++ SuperHexagon/Game.fs
@@ -14,7 +14,11 @@ type Obstacle =
 
 module Obstacles =
   // Configurations in easy mode
-  let easyGroups = [ [  1;2;3;4;5]; [0;1;2;  4;5]; [0;2;4]; [1;3;5] ] |> List.map (List.map (fun s -> { section = s; distance = 2.}))
+  let easyGroups =
+    [ [  1;2;3;4;5]; [0;1;2;  4;5]
+      [0;2;4];       [1;3;5]
+      [0;1;3;4];     [1;2;4;5];     [2;3;5;0] ]
+      |> List.map (List.map (fun s -> { section = s; distance = 2.}))
   
   let spawnGroup rand = easyGroups.[Seq.head rand |> int |> wrap easyGroups.Length]
   
